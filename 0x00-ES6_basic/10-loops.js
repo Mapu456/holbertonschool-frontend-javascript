5c2d689536377a03a1a8368b51587c163e4f4e01
Mapu456 <mpaula456@hotmail.com>
2022-04-11 21:07:30 -0500

    adding comments

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
   const array2 = [];
   for (const value of array) {
--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,5 +1,6 @@
 import getBudgetObject from "./7-getBudgetObject";
 
+/* eslint-disable */
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
