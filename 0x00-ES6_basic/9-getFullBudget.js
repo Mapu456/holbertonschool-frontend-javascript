0a1d0ccd8a98c6a3c06e5e8376db7c45ef87f91c
Mapu456 <mpaula456@hotmail.com>
2022-04-11 22:08:27 -0500

    adding comments

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,6 +1,6 @@
+/* eslint-disable */
 import getBudgetObject from "./7-getBudgetObject";
 
-/* eslint-disable */
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
